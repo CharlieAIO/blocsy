1f7dfd8e28e89bb4476fdca63811b42359610e04
CharlieAIO <charliebottomley15@gmail.com>
2024-11-17 21:26:41 +0000

    mongo functions

--- internal/db/mongo_repo.go
+++ internal/db/mongo_repo.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"context"
+	"defi-intel/internal/solana"
 	"defi-intel/internal/types"
 	"fmt"
 	"go.mongodb.org/mongo-driver/bson"
@@ -57,4 +58,100 @@ func (repo *MongoRepository) StoreToken(ctx context.Context, token types.Token) 
 	}
 
 	return nil
+}
+
+func (repo *MongoRepository) LookupByToken(ctx context.Context, address string, network string) (types.Token, []types.Pair, error) {
+	token, err := repo.lookupToken(ctx, address, network)
+	if err != nil {
+		return types.Token{}, nil, fmt.Errorf("cannot find token: %w", err)
+	}
+
+	if token.Address == "" {
+		return types.Token{}, []types.Pair{}, nil
+	}
+
+	filter := bson.M{
+		"token":   token.Address,
+		"network": token.Network,
+	}
+
+	cur, err := repo.pairsCollection.Find(ctx, filter, nil)
+	if err != nil {
+		return token, nil, nil
+	}
+
+	var pairs []types.Pair
+	if err := cur.All(ctx, &pairs); err != nil {
+		return types.Token{}, nil, fmt.Errorf("cannot decode pairs: %w", err)
+	}
+
+	return token, pairs, nil
+}
+
+func (repo *MongoRepository) lookupToken(ctx context.Context, address string, network string) (types.Token, error) {
+	filer := bson.M{"address": address, "network": network}
+
+	res := repo.tokensCollection.FindOne(ctx, filer)
+	if err := res.Err(); err != nil {
+		if err == mongo.ErrNoDocuments {
+			return types.Token{}, nil
+		}
+
+		return types.Token{}, fmt.Errorf("cannot find token: %w", err)
+	}
+
+	var token types.Token
+	if err := res.Decode(&token); err != nil {
+		return types.Token{}, fmt.Errorf("cannot decode token: %w", err)
+	}
+
+	return token, nil
+}
+
+func (repo *MongoRepository) LookupByPair(ctx context.Context, address string, network string, tf solana.TokenFinder) (types.Pair, types.Token, types.QuoteToken, error) {
+	filter := bson.M{
+		"address": address,
+		"network": network,
+	}
+
+	cur, err := repo.pairsCollection.Find(ctx, filter, nil)
+	if err != nil {
+		return types.Pair{}, types.Token{}, types.QuoteToken{}, fmt.Errorf("cannot find pair: %w", err)
+	}
+
+	var pairs []types.Pair
+	if err := cur.All(ctx, &pairs); err != nil {
+		return types.Pair{}, types.Token{}, types.QuoteToken{}, fmt.Errorf("cannot decode pairs: %w", err)
+	}
+
+	if len(pairs) == 0 {
+		return types.Pair{}, types.Token{}, types.QuoteToken{}, nil
+	}
+
+	token, err := tf.FindToken(ctx, pairs[0].Token)
+	if err != nil {
+		return types.Pair{}, types.Token{}, types.QuoteToken{}, fmt.Errorf("cannot find token: %w", err)
+	}
+
+	if token.Address == "" {
+		//When no pair found return empty pair and token and caller can trigger a function to find pair and store it.
+		return types.Pair{}, types.Token{}, types.QuoteToken{}, nil
+	}
+
+	quoteToken, err := tf.FindToken(ctx, pairs[0].QuoteToken.Address)
+	if err != nil {
+		return types.Pair{}, types.Token{}, types.QuoteToken{}, fmt.Errorf("cannot find quote token: %w", err)
+	}
+
+	if quoteToken.Address == "" {
+		return types.Pair{}, types.Token{}, types.QuoteToken{}, nil
+	}
+
+	return pairs[0], *token, types.QuoteToken{
+		Identifier: pairs[0].QuoteToken.Identifier,
+		Name:       quoteToken.Name,
+		Symbol:     quoteToken.Symbol,
+		Address:    quoteToken.Address,
+		Decimals:   quoteToken.Decimals,
+	}, nil
 }
