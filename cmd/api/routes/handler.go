d1b0342975debe6b0da8d20421a229b1634558b0
CharlieAIO <charliebottomley15@gmail.com>
2024-12-03 17:40:05 +0000

    add route to handler

--- cmd/api/routes/handler.go
+++ cmd/api/routes/handler.go
@@ -50,6 +50,7 @@ func (h *Handler) GetHttpHandler() http.Handler {
 		r.Get("/pair/{pair}", h.PairLookupHandler)
 		r.Get("/token/{token}", h.TokenLookupHandler)
 
+		r.Get("/check-bundled/{token}", h.CheckBundledHandler)
 		r.Get("/find-swap/{token}/{amount}/{timestamp}", h.FindSwapHandler)
 
 		r.Get("/pnl-aggregated/{wallet}", h.AggregatedPnlHandler)
