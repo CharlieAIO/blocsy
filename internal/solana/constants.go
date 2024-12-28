f8b4cbc453c2f613704980b685fbbf21512a76cc
CharlieAIO <charliebottomley15@gmail.com>
2024-12-28 12:08:07 +0000

    ignores

--- internal/solana/constants.go
+++ internal/solana/constants.go
@@ -40,6 +40,7 @@ var IgnoreToUsers = map[string]bool{
 	"5PvhfrBkVsmXqKvo4vvHArLxzgGz9WXqaE9QPeLKodrj": true,
 	"CpoD6tWAsMDeyvVG2q2rD1JbDY6d4AujnvAn2NdrhZV2": true,
 	"5ioytVEBHJKxbekKwAYMfyu6GXbFmFaVnQGa3agUutMG": true,
+	"JD25qVdtd65FoiXNmR89JjmoJdYk9sjYQeSTZAALFiMy": true,
 }
 
 var IgnoreTokens = map[string]bool{
